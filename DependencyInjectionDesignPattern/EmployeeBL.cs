d33b348649c1411efbefd77a461a24885012f144
Shahhoseini.Maziar <Shahhoseini.Maziar@SGS001P10361>
2024-01-08 19:19:57 +0330

    Add project files.

---
+++ DependencyInjectionDesignPattern/EmployeeBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDesignPattern
+{
+    public class EmployeeBL
+    {
+        public EmployeeBL()
+        {
+                
+        }
+        //For Constructor Dependency Injection
+        public EmployeeBL(IEmployeeDAL _employeeDAL)
+        {
+            employeeDAL = _employeeDAL;
+        }
+        //For Property Dependency Injection
+        public IEmployeeDAL EmployeeDataObject
+        {
+            set
+            {
+                this.employeeDAL = value;
+            }
+        }
+
+
+        public IEmployeeDAL employeeDAL;
+        public List<Employee> GetAllEmployees()
+        {
+            //Creating an Instance of Dependency Class means it is a Tight Coupling
+            //employeeDAL = new EmployeeDAL();
+            return employeeDAL.SelectAllEmployees();
+        }
+        
+        //For Method Dependency Injection
+        public List<Employee> GetAllEmployees(IEmployeeDAL employeeDAL)
+        {
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
+
---
+++ DependencyInjectionDesignPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInjectionDesignPattern;
+
+Console.WriteLine("Hello, World!");
+
+#region Constructor Dependency Injection
+EmployeeBL employeeBL = new EmployeeBL(new EmployeeDAL());
+#endregion
+
+
+#region Property Dependency Injection
+EmployeeBL employeeBL1 = new EmployeeBL();
+employeeBL1.employeeDAL = new  EmployeeDAL();
+#endregion
+
+List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+foreach (Employee emp in ListEmployee)
+{
+    Console.WriteLine($"ID = {emp.ID}, Name = {emp.Name}, Department = {emp.Department}");
+}
+
+#region Method Dependency Injection
+List<Employee> ListEmployee1 = employeeBL1.GetAllEmployees(new EmployeeDAL());
+
+#endregion
+
+Console.ReadKey();
